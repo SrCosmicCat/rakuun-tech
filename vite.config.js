295bcb2db7dc3dc9acf7d3bdce836094ba2fa8b5
SrCosmicCat <gatitosplus000@gmail.com>
2024-11-19 01:40:29 -0600

    pwa test 2

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: './',
-  plugins: [react(), VitePWA(PWAmanifest)],
-})
-
 const PWAmanifest = {
   registerType: 'prompt',
   includeAssets: ['favicon.ico'],
@@ -17,15 +11,21 @@ const PWAmanifest = {
     description: 'Tu tienda de tecnología',
     icons: [
       {
-        src: "windows11/Square44x44Logo.targetsize-256.png",
-        sizes: "256x256",
-        type: "image/png"
+        src: '/android-launchericon-512-512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'favicon'
       },
       {
-        src: "android/android-launchericon-512-512.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "any maskable"
+        src: '/Square44x44Logo.targetsize-256.png',
+        sizes: '256x256',
+        type: 'image/png'
+      },
+      {
+        src: '/android-launchericon-512-512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any maskable'
       },
       
     ],
@@ -37,3 +37,10 @@ const PWAmanifest = {
     orientation: 'portrait',
   }
 }
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [react(), VitePWA(PWAmanifest)],
+})
+
